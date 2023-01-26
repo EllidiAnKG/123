04e0c8473c5864193b6bf910a035a6d5d6c297e0
EllidiAnKG <112854201+EllidiAnKG@users.noreply.github.com>
2023-01-26 12:28:12 +0500

    Add files via upload

---
+++ 32131231/view/vipupu.js
@@ -0,0 +1,29 @@
+const pageContacts=`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <h1>Контакты</h1>
+        </li>
+        <li>
+            mail
+        </li>
+        <li>
+            telegram
+        </li>
+        <li>
+            vk
+        </li>
+    </ul>
+</body>
+</html>
+`
+module.exports=pageContacts;
